6b9fcfdf5aa95fb27cd5eec426dd4a306bf3e1ff
Samuel Ivan <maztersamzz@gmail.com>
2021-10-11 06:35:10 +0700

    Course 43 - POST/ Register a New User REST API

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,14 +1,38 @@
-const {User} = require('../models/user');
-const express = require('express');
-const router = express.Router();
+const { User } = require('../models/user')
+const express = require('express')
+const router = express.Router()
 
-router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+router.get(`/`, async (req, res) => {
+	const userList = await User.find()
 
-    if(!userList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(userList);
+	if (!userList) {
+		res.status(500).json({ success: false })
+	}
+	res.send(userList)
 })
 
-module.exports =router;
+// ==================== Store Category ====================
+router.post('/', async (req, res) => {
+	let user = new User({
+		name: req.body.name,
+		email: req.body.email,
+		passwordHash: req.body.passwordHash,
+		phone: req.body.phone,
+		isAdmin: req.body.isAdmin,
+		street: req.body.street,
+		apartement: req.body.apartement,
+		zip: req.body.zip,
+		city: req.body.city,
+		country: req.body.country,
+		color: req.body.color,
+	})
+
+	user = await user.save()
+
+	if (!user) return res.status(404).send('the user cannot be created')
+
+	res.send(user)
+})
+// ==================== /Store Category ====================
+
+module.exports = router
