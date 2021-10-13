fe53ea29885e24d7482c4710692102e536d45ef9
Samuel Ivan <maztersamzz@gmail.com>
2021-10-13 14:40:46 +0700

    Course 52 - Get User Count REST API

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -75,4 +75,38 @@ router.post('/login', async (req, res) => {
 })
 // ==================== /Login User ====================
 
+// ==================== Delete User ====================
+// api/v1/:id
+router.delete('/:id', (req, res) => {
+	User.findByIdAndRemove(req.params.id)
+		.then((user) => {
+			if (user) {
+				return res
+					.status(200)
+					.json({ success: true, message: 'the user is deleted!' })
+			} else {
+				return res
+					.status(404)
+					.json({ success: false, message: 'user not found!' })
+			}
+		})
+		.catch((err) => {
+			return res.status(400).json({ success: false, error: err })
+		})
+})
+// ==================== /Delete User ====================
+
+// ==================== Get Count User ====================
+router.get(`/get/count`, async (req, res) => {
+	const userCount = await User.countDocuments()
+
+	if (!userCount) {
+		res.status(500).json({ success: false })
+	}
+	res.send({
+		userCount: userCount,
+	})
+})
+// ==================== /Get Count User ====================
+
 module.exports = router
