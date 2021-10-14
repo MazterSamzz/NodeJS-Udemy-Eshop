4bf32a3178ccb3ca80f74e92f027056aebd3d9b2
Samuel Ivan <maztersamzz@gmail.com>
2021-10-14 07:14:38 +0700

    Course 59 & 60 - Section 6 Orders - Delete Order Items after Deleting the Order

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -70,4 +70,42 @@ router.post('/', async (req, res) => {
 })
 // ==================== /Store Order ====================
 
+// ==================== Update Order ====================
+router.put('/:id', async (req, res) => {
+	const order = await Order.findByIdAndUpdate(
+		req.params.id,
+		{
+			status: req.body.status,
+		},
+		{ new: true }
+	)
+	if (!order) return res.status(404).send('the order cannot be created')
+	res.send(order)
+})
+// ==================== /Update Order ====================
+
+// ==================== Delete Order ====================
+// api/v1/:id
+router.delete('/:id', (req, res) => {
+	Order.findByIdAndRemove(req.params.id)
+		.then(async (order) => {
+			if (order) {
+				await order.orderItems.map(async (orderItem) => {
+					await OrderItem.findByIdAndRemove(orderItem)
+				})
+				return res
+					.status(200)
+					.json({ success: true, message: 'the order is deleted!' })
+			} else {
+				return res
+					.status(404)
+					.json({ success: false, message: 'order not found!' })
+			}
+		})
+		.catch((err) => {
+			return res.status(400).json({ success: false, error: err })
+		})
+})
+// ==================== /Delete Order ====================
+
 module.exports = router
