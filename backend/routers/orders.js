e57faf393680e6d4c970f6f92f0359fe427976f5
Samuel Ivan <maztersamzz@gmail.com>
2021-10-15 04:44:22 +0700

    Course 62 - Section 6 Orders - Get Total E-shop Sales using

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -122,4 +122,30 @@ router.delete('/:id', (req, res) => {
 })
 // ==================== /Delete Order ====================
 
+// ==================== Total Sales ====================
+router.get('/get/totalsales', async (req, res) => {
+	const totalSales = await Order.aggregate([
+		{ $group: { _id: null, totalsales: { $sum: '$totalPrice' } } },
+	])
+
+	if (!totalSales) {
+		return res.status(400).send('The order sales cannot be generated')
+	}
+	res.send({ totalsales: totalSales.pop().totalsales })
+})
+// ==================== /Total Sales ====================
+
+// ==================== Get Count Order ====================
+router.get(`/get/count`, async (req, res) => {
+	const orderCount = await Order.countDocuments()
+
+	if (!orderCount) {
+		res.status(500).json({ success: false })
+	}
+	res.send({
+		orderCount: orderCount,
+	})
+})
+// ==================== /Get Count Order ====================
+
 module.exports = router
